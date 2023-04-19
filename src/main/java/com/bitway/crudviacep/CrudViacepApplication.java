a15d479ea6b64b67d3f510ff6ad3e59da0d3b6b9
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2023-04-19 14:54:35 -0300

    Swagger adicionado

--- src/main/java/com/bitway/crudviacep/CrudViacepApplication.java
+++ src/main/java/com/bitway/crudviacep/CrudViacepApplication.java
@@ -2,8 +2,10 @@ package com.bitway.crudviacep;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CrudViacepApplication {
 
 	public static void main(String[] args) {
